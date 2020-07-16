68be11e0c39ac364286fdca4188eb2e3afcbdde0
kaiquealbuquerque <kaiquealbuqueque@hotmail.com>
2020-07-16 20:04:43 -0300

    interações continuação

--- scr/pages/Interactions/ListInteractionsScreen.js
+++ scr/pages/Interactions/ListInteractionsScreen.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState, PureComponent } from 'react';
 
-import { View, ScrollView, StyleSheet, Alert } from 'react-native';
+import { View, ActivityIndicator, FlatList, StyleSheet, Alert, TouchableWithoutFeedback } from 'react-native';
 import { Card, Header, Text, Icon, Avatar } from 'react-native-elements';
 
 import { AutoGrowingTextInput } from 'react-native-autogrow-textinput';
@@ -10,136 +10,191 @@ import COLORS from '../../styles/Colors.js';
 import CrudService from '../../services/Crud/CrudService.js';
 import moment from 'moment';
 
+class MyListItem extends PureComponent {
+	render() {
+        
+        let textInteraction = "";
+        let initials = "";
+        let email = this.props.interaction.login.toLowerCase();
+        let arrInitials = this.props.interaction.name.split(" ");
+        let render;
+
+        if(arrInitials.length >= 2)
+            initials = `${arrInitials[0].substr(0,1).toUpperCase()}${arrInitials[1].substr(0,1).toUpperCase()}`;
+        else if(arrInitials.length == 1)
+            initials = arrInitials[0].substr(0,2).toUpperCase();
+
+        textInteraction = this.props.interaction.comment.replace("\\r\\n", "{'\\n'}");
+
+        if (this.props.interaction.userTypeId != 1) {
+            render = <View style={{ marginBottom:5 }}>
+                        <View style={{flexDirection: 'row', marginLeft:15, marginTop:15}}>
+                            <View style={{flex:1}}>
+                                <Avatar size="medium" rounded title={initials} />                        
+                            </View>
+                            <View style={{flex:5, justifyContent:'center'}}>
+                                <Text style={{fontSize:15, fontWeight: 'bold'}}>{this.props.interaction.name}</Text>
+                                <Text style={{fontSize:10, fontWeight: 'bold'}}>{email}</Text>
+                                <Text style={{fontSize:10, fontWeight: 'bold'}}>{moment(this.props.interaction.dateHour).format("DD/MM/YYYY HH:mm")}</Text>
+                            </View>
+                        </View>
+                        <Card containerStyle={{borderRadius:15}}>
+                            <Text style={{fontSize:15}}>{textInteraction}</Text>
+                        </Card>
+                    </View> 
+        }
+        else{
+            render = <View style={{ marginBottom:5 }}>
+                        <View style={{flexDirection: 'row-reverse', marginStart:8, marginTop:15}}>
+                            <View style={{flex:1}}>
+                                <Avatar size="medium" rounded title={initials} />                        
+                            </View>
+                            <View style={{flex:5, justifyContent:'center', alignItems:'flex-end', marginRight:15}}>
+                                <Text style={{fontSize:15, fontWeight: 'bold'}}>{this.props.interaction.name}</Text>
+                                <Text style={{fontSize:10, fontWeight: 'bold'}}>{email}</Text>
+                                <Text style={{fontSize:10, fontWeight: 'bold'}}>{moment(this.props.interaction.dateHour).format("DD/MM/YYYY HH:mm")}</Text>
+                            </View>
+                        </View>
+                        <Card containerStyle={{borderRadius:15}}>
+                            <Text style={{fontSize:15}}>{textInteraction}</Text>
+                        </Card>
+                    </View>
+        }
+		return (
+			<>
+                {render}
+            </>
+	  	)
+	}
+}
+
 export default function ListInteractionsScreen(props){
     
     let crudService = new CrudService();
-    const [listInteractions, setListInteractions] = useState([]);
+    const [listInteractions, setListInteractions] = useState(
+        {
+            data: [], 
+			page: 0,
+			loading: false,
+        }
+    );
     
+	const [refreshing, setRefreshing] = useState(false);
+
+    const [onEndReachedCalledDuringMomentum , setOnEndReachedCalledDuringMomentum ] = useState(false);
+
     useEffect(() => {
 		
 		async function getInteractionsInitial(){
-			await getInteractions();
+            await getInteractions();
+            setRefreshing(false);
 		}
 
 		getInteractionsInitial();
-	});
+	}, [refreshing]);
 
 	const getInteractions = async () => {
         
-        let result = await crudService.get(`interactions/${props.navigation.state.params.demandsId}`, props.navigation.state.params.userData.token);
-        
-        if(result.status == 401){
-            Alert.alert(
-                "Sessão Expirada",
-                "Sua sessão expirou. Por favor, realize o login novamente.",
-                [
-                    {
-                        text: "Ok",
-                        onPress: () => props.navigation.navigate('LoginEmailScreen'),
-                        style: "ok"
-                    }
-                ],
-                { cancelable: false }
-            );
-        }
-        else if(result.status == 400){
-            Alert.alert(
-                "Erro",
-                resultContacts.data[0],
-                [
-                    {
-                        text: "Ok",
-                        onPress: () => props.navigation.navigate('DemandsListScreen'),
-                        style: "ok"
-                    }
-                ],
-                { cancelable: false }
-            );
-        }
-        else{
-            setListInteractions(result.data);
-        }
-	}
+        if (!onEndReachedCalledDuringMomentum) {
 
-    var lista = [];
-    listInteractions.forEach((interaction, i) => {
+            if (listInteractions.loading) return;
+            
+            setListInteractions({
+				data: [...listInteractions.data],
+				page: listInteractions.page,
+				loading: refreshing == true ? false : true,
+            })
+            
+            let result = await crudService.get(`interactions?DemandsId=${props.navigation.state.params.demandsId}&page=${listInteractions.page}`, props.navigation.state.params.userData.token);
+            
+            if(result.status == 401){
+                Alert.alert(
+                    "Sessão Expirada",
+                    "Sua sessão expirou. Por favor, realize o login novamente.",
+                    [
+                        {
+                            text: "Ok",
+                            onPress: () => props.navigation.navigate('LoginEmailScreen'),
+                            style: "ok"
+                        }
+                    ],
+                    { cancelable: false }
+                );
+            }
+            else if(result.status == 400){
+                Alert.alert(
+                    "Erro",
+                    resultContacts.data[0],
+                    [
+                        {
+                            text: "Ok",
+                            onPress: () => props.navigation.navigate('DemandsListScreen'),
+                            style: "ok"
+                        }
+                    ],
+                    { cancelable: false }
+                );
+            }
+            else{
+                setListInteractions(result.data);
 
-        let textInteraction = "";
-        let initials = "";
-        let email = interaction.login.toLowerCase();
-        let arrInitials = interaction.name.split(" ");
-        let hello = "";
-        
-        if(arrInitials.length >= 2){
-            initials = `${arrInitials[0].substr(0,1).toUpperCase()}${arrInitials[1].substr(0,1).toUpperCase()}`;
-            hello = arrInitials[0].toUpperCase();
-        }
-        else if(arrInitials.length == 1){
-            initials = arrInitials[0].substr(0,2).toUpperCase();
-            hello = arrInitials[0].toUpperCase();
-        }
+                setListInteractions({
+					data: [...listInteractions.data, ...result.data],
+					page: listInteractions.page + 25,
+					loading: false,
+				})	
+            }
 
-        textInteraction = interaction.comment.replace("\\r\\n", "{'\\n'}");
-
-        if (interaction.userTypeId != 1) {
-            lista.push(
-                <View style={{ marginBottom:5 }}>
-                    <View style={{flexDirection: 'row', marginLeft:15, marginTop:15}}>
-                        <View style={{flex:1}}>
-                            <Avatar size="medium" rounded title={initials} />                        
-                        </View>
-                        <View style={{flex:5, justifyContent:'center'}}>
-                            <Text style={{fontSize:15, fontWeight: 'bold'}}>{interaction.name}</Text>
-                            <Text style={{fontSize:10, fontWeight: 'bold'}}>{email}</Text>
-                            <Text style={{fontSize:10, fontWeight: 'bold'}}>{moment(interaction.dateHour).format("DD/MM/YYYY HH:mm")}</Text>
-                        </View>
-                    </View>
-                    <Card containerStyle={{borderRadius:30}}>
-                        <Text style={{fontSize:15}}>{textInteraction}</Text>
-                    </Card>
-                </View>  
-            )      
+            setOnEndReachedCalledDuringMomentum(true);
         }
-        else{
-            lista.push(
-                <View style={{ marginBottom:5 }}>
-                    <View style={{flexDirection: 'row-reverse', marginRight:20, marginTop:15}}>
-                        <View style={{flex:1}}>
-                            <Avatar size="medium" rounded title={initials} />                        
-                        </View>
-                        <View style={{flex:5, justifyContent:'center', alignItems:'flex-end', marginRight:15}}>
-                            <Text style={{fontSize:15, fontWeight: 'bold'}}>{interaction.name}</Text>
-                            <Text style={{fontSize:10, fontWeight: 'bold'}}>{email}</Text>
-                            <Text style={{fontSize:10, fontWeight: 'bold'}}>{moment(interaction.dateHour).format("DD/MM/YYYY HH:mm")}</Text>
-                        </View>
-                    </View>
-                    <Card containerStyle={{borderRadius:30}}>
-                        <Text style={{fontSize:15}}>{textInteraction}</Text>
-                    </Card>
-                </View> 
-            )
-        }
-    });
+    }
+    
+    const renderFooter = () => {
+		if (!listInteractions.loading) return null;
+			return (
+				<View style={styles.loading}>
+					<ActivityIndicator />
+				</View>
+		);
+    };
+    
+    const renderItem = ({ item }) => (
+		<MyListItem
+			interaction={item}
+    	/>
+	);
 
     return(
         <View style={{flex:1}}>
             <View style={{flex:7}}>
                 <Header
-                    leftComponent={<Icon
-                                    name='keyboard-backspace'
-                                    color='#fff'
-                                    onPress={() => this.props.navigation.navigate('DemandsList')} />}
-                    centerComponent={<Text h4 style={{textAlign: 'center', color: '#fff'}}>Interações</Text>}
-                    containerStyle={{
-                        backgroundColor: COLORS.default,
-                        paddingTop: 0
-                }}
+                    leftComponent={<TouchableWithoutFeedback onPress={() => props.navigation.navigate('DemandsList')}>
+                                        <Icon
+                                            name='keyboard-backspace'
+                                            color='#fff'
+                                        />
+                                    </TouchableWithoutFeedback>
+                                    }
+                                    centerComponent={<Text h4 style={{textAlign: 'center', color: '#fff'}}>Interações</Text>}
+                                    containerStyle={{
+                                        backgroundColor: COLORS.default,
+                                        paddingTop: 0
+                                    }}
                 />
-                <ScrollView>
-                    {lista}
-                </ScrollView>
+                <FlatList
+                    inverted={true}
+                    style={{ flex: 1.8, marginBottom: 20 }}
+                    contentContainerStyle={ styles.list }
+                    data={ listInteractions.data }
+                    renderItem={ renderItem }
+                    keyExtractor={(item, index) => item.interactionId.toString()}
+                    onEndReached={ getInteractions }
+                    onEndReachedThreshold={ 0.1 }
+                    onMomentumScrollBegin={() => { setOnEndReachedCalledDuringMomentum(false) }}
+                    ListFooterComponent={ renderFooter }
+                />
             </View>
-            <View style={{flex:1, flexDirection: 'row', marginTop:20, marginBottom:5, backgroundColor: "#efefef", borderRadius:40}}>
+            <View style={{flex:1, flexDirection: 'row', marginLeft: 10, marginRight: 10, marginBottom: 10, backgroundColor: "#efefef", borderRadius:20}}>
                 <View style={{flex:10, paddingLeft:10, justifyContent: 'center'}}>
                     <AutoGrowingTextInput placeholder={'Digite sua Interação'} />                
                 </View>
@@ -154,31 +209,6 @@ export default function ListInteractionsScreen(props){
     );
 }
 
-function UselessTextInput(props) {
-    return ( 
-      <TextInput style={styles.textArea}
-        placeholder="Digite a interação..."
-        placeholderTextColor="grey"
-        {...props} 
-        editable
-      />
-    );
-  }
-  
-function UselessTextInputMultiline() {
-    const [value, onChangeText] = React.useState('');
-    return(
-        <View style={styles.textAreaContainer}>
-            <UselessTextInput
-                multiline
-                numberOfLines={4}
-                onChangeText={text => onChangeText(text)}
-                value={value}
-            />
-        </View>
-    );
-}
-
 const styles = StyleSheet.create({
     textAreaContainer: {
       borderColor: '#bdc3c7',
@@ -188,5 +218,12 @@ const styles = StyleSheet.create({
     textArea: {
       height: 150,
       justifyContent: "flex-start"
-    }
+    },
+    loading: {
+		alignSelf: 'center',
+		marginVertical: 20,
+    },
+    list: {
+		paddingHorizontal: 20,
+	}
   })
